54d4fe0cbc59df0165f21a6a8611430fa64de817
Juan lozano <jandresl092@gmail.com>
2024-09-27 19:26:26 -0500

    first commit

---
+++ EJERCICIOS JAVASCRIPT/javascript.js
@@ -0,0 +1,8 @@
+
+
+var nota_1 = parseInt(prompt("ingresar la nota 1"))
+var nota_2 = parseInt(prompt("ingresar la nota 2"))
+var nota_3 = parseInt(prompt("ingresar la nota 3"))
+        
+var promedio = (nota_1 + nota_2 + nota_3)/3
+document.write("El promerdio de las notas es: " + promedio)
